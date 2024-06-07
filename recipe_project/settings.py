52eb675ebb88ead8bb058e64ca07499917f3b9ac
Argos Stone <peter@raedangroup.com>
2024-06-06 23:14:46 -0400

    Update ALLOWED_HOSTS for Heroku

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'sheltered-woodland-62724.herokuapp.com']
 
 
 # Application definition
